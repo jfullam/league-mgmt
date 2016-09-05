581f7478ae04d26f6f715e126212081a6022657e
Jonathan Fullam <jonathanfullam@gmail.com>
2016-09-05 18:21:03 -0400

    update port for person-service

--- zuul-proxy/src/main/java/org/jfullam/ZuulProxyApplication.java
+++ zuul-proxy/src/main/java/org/jfullam/ZuulProxyApplication.java
@@ -3,11 +3,13 @@ package org.jfullam;
 import org.jfullam.filters.pre.LoggingPreFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableEurekaClient
 public class ZuulProxyApplication {
 
 	public static void main(String[] args) {
