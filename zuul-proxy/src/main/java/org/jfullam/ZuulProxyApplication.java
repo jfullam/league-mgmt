60e439c158afead7c6191b21f61ecf0e20bff7d9
Jonathan Fullam <jonathanfullam@gmail.com>
2016-09-05 16:14:13 -0400

    competition-service and person-service

---
+++ zuul-proxy/src/main/java/org/jfullam/ZuulProxyApplication.java
@@ -0,0 +1,17 @@
+package org.jfullam;
+
+import org.jfullam.filters.pre.LoggingPreFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulProxyApplication.class, args);
+	}
+
+}
