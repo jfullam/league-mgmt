6722b343fd59ceb3e727c841a8ebbd7246f60a88
Jonathan Fullam <jonathanfullam@gmail.com>
2016-09-06 16:02:10 -0400

    config server files

--- zuul-proxy/src/main/java/org/jfullam/ZuulProxyApplication.java
+++ zuul-proxy/src/main/java/org/jfullam/ZuulProxyApplication.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
 public class ZuulProxyApplication {
 
 	public static void main(String[] args) {
